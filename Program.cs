fd1f5f3a6964043e3a0ab8e2e650c61dac9da9b1
Mahsa Agharezaei <mahsa.agharezaee@gmail.com>
2022-07-16 16:04:08 +0430

    add class library

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffeeShop.DataAccess;
 
 namespace WiredBrainCoffeeShopInfoTool
 {
@@ -7,6 +8,23 @@ namespace WiredBrainCoffeeShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee Shop Info Tool");
+            Console.WriteLine("Write help to list available commands");
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available Coffee Shops commands: ");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
         }
+                
+                
     }
 }
